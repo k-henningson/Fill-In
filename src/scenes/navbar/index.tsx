9cadeab7332a3736f58ee026afb17e9f2b0ad4e7
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-31 15:05:59 -0700

    create custom button component

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -55,7 +56,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Join Now!</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Join Now!
+                  </ActionButton>
                 </div>
               </div>
             ) : (
