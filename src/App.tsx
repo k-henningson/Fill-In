1cd873142775c6edcc0ee8b30f918a18a4eeed8e
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-31 11:11:59 -0700

    select inital page colours and set up in tailwind config, index css, and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
-  return <div className="App">Fill in</div>;
+  return (
+    <div className="app bg-beige">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        beige: "#f5f4ef",
+        charcoal: "#423a37",
+        peach: "#c49683",
+      },
+      backgroundImage: (theme) => ({
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
 };
